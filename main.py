fd40c472ff05539f8745fd31a2b8908a72af6f61
Denys Zinoviev <reiisbetterthanasuka@gmail.com>
2023-04-05 01:34:33 +0200

    Improvement

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from warden import Warden
+from logger import Logger
 
 if __name__ == '__main__':
-    Warden().watch()
+    try:
+        Warden().watch()
+    except Exception as e:
+        message = "Exception during the warden's watch:\n" + str(e)
+        Logger().log(message)
