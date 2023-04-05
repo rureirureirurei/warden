5934c5d16da54a559cde92b68caf96067a453981
Denys Zinoviev <reiisbetterthanasuka@gmail.com>
2023-04-05 22:45:48 +0200

    Removed debugging output

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from web3 import Web3#TODO REMOVE
 
 if __name__ == '__main__':
     try:
-        w3 = Web3(Web3.HTTPProvider(config.INFURA_KEY))#TODO REMOVE
-        Sender(w3).send_max(config.PRIVATE_KEY, config.PUBLIC_KEY, config.RECEIVER)
-        #Warden().watch()
+        Warden().watch()
     except Exception as e:
         message = "Exception during the warden's watch:\n" + str(e)
         Logger().log(message)
