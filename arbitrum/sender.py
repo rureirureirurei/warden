f12d489ef03200f60e0cf4714bf78a9a1e4d324c
Denys Zinoviev <reiisbetterthanasuka@gmail.com>
2023-04-21 00:00:14 +0200

    Moved the actual implementation from the eth package

--- arbitrum/sender.py
+++ arbitrum/sender.py
@@ -1,3 +1,4 @@
+from web3 import Web3, HTTPProvider
 from logger import Logger
 
 
@@ -6,14 +7,14 @@ class Sender:
         self.w3 = w3
 
     # BOTH VALUE AND GAS IN WEI
-    def send(self, private_key, public_key, receiver, value, gas):
+    def send(self, private_key, public_key, receiver, value, gas, intrinsic=21000):
         try:
             nonce = self.w3.eth.get_transaction_count(public_key)
             tx = {
                 'nonce': nonce,
                 'to': receiver,
                 'value': value,
-                'gas': 21000,
+                'gas': intrinsic,
                 'gasPrice': gas,
             }
 
@@ -21,13 +22,15 @@ class Sender:
             tx_hash = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
             message = f'\nSent {value} wei\nfrom {public_key} to {receiver}\nwith gas {gas}\ntx hash: {tx_hash}'
         except Exception as e:
-            message = f'\nTried to send {value} wei\nfrom {public_key} to {receiver}\nwith gas {gas}\nbut caught exeption:\n' + str(
-                e)
+            message = f'\nTried to send {value} wei\nfrom {public_key} to {receiver}\nwith gas {gas}\nbut caught exeption:\n' + str(e)
         Logger().log(message)
 
     def send_max(self, private_key, public_key, receiver):
         balance = self.w3.eth.get_balance(public_key)
-        gas = self.w3.to_wei('50', 'gwei')  # TODO make smart
-        epsilon = self.w3.to_wei('0.0015', 'ether')
+
+        gas = self.w3.to_wei('110000000', 'wei')
+        intrinsic = self.w3.to_wei('800000', 'wei')
+        epsilon = self.w3.to_wei('90000', 'gwei')
         available = balance - gas - epsilon
-        self.send(private_key, public_key, receiver, available, gas)
+
+        self.send(private_key, public_key, receiver, available, gas, intrinsic)
